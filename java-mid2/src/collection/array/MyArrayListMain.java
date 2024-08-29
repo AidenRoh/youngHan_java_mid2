d211257daadc048ab74ac66790eea88b982635bb
AidenRoh <colohrby@gmail.com>
2024-08-29 23:42:57 +0900

    feature: add customize array list 1

---
+++ java-mid2/src/collection/array/MyArrayListMain.java
@@ -0,0 +1,34 @@
+package collection.array;
+
+public class MyArrayListMain {
+
+    public static void main(String[] args) {
+        MyArrayListV1 list = new MyArrayListV1();
+        System.out.println(" == data add ==");
+        System.out.println(list);
+        list.add("a");
+        System.out.println(list);
+        list.add("b");
+        System.out.println(list);
+        list.add("c");
+        System.out.println(list);
+
+
+        System.out.println(" == function ==");
+        System.out.println("list.size() = " + list.size());
+        System.out.println("list.get(1) = " + list.get(1));
+        System.out.println("list.indexOf('c') = " + list.indexOf("c"));
+        System.out.println("list.set(2, 'z') = " + list.set(2, "z"));
+        System.out.println(list);
+
+        System.out.println( " == out of accepting point ==");
+        list.add("a");
+        System.out.println(list);
+        list.add("e");
+        System.out.println(list);
+
+        //범위 초과, capacity가 늘어나지 않으면 예외 발생
+        list.add("f");
+        System.out.println(list  );
+    }
+}
